2c460b66a24ed14c8d856bb8438b2065a5e11206
berry82d <d.berry.hd@gmail.com>
2026-09-08 19:53:21 -0400

    LAW 8 Law of Logs restore c166472 No issues - 65 line bills_tab no snippets

--- lib/services/app_state.dart
+++ lib/services/app_state.dart
@@ -1,4 +1,4 @@
-﻿// lib/services/app_state.dart
+// lib/services/app_state.dart
 import 'package:flutter/material.dart';
 import 'package:haven_os/core/storage/storage_service.dart';
 import 'package:haven_os/domain/services/finance_service.dart';
