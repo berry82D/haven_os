db5862738993db1d1ed5a4cefad5404d81316a99
berry82d <d.berry.hd@gmail.com>
2026-09-08 19:13:00 -0400

    fix: ledger const TabBarView unwrap restore c166472 20 wrappers No issues final

--- lib/features/cfo/widgets/ledger_screen.dart
+++ lib/features/cfo/widgets/ledger_screen.dart
@@ -25,7 +25,7 @@ class LedgerScreen extends StatelessWidget {
             indicatorColor: HavenColors.green,
           ),
         ),
-        body: const TabBarView(
+        body: TabBarView(
           children: [
             BillsTab(),
             TransactionsTab(),
